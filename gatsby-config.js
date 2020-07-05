5c414159c1f796e65cae0026163611cb82ae2ab5
hnishi <nishigami2509@gmail.com>
2020-07-06 00:37:02 +0900

    finish tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,44 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Your Site Title`,
+    description: `A description for your blazing fast site, using multiple themes!`,
+    author: `Your name`,
+    menuLinks: [
+      {
+        name: `Blog`,
+        url: `/`,
+      },
+      {
+        name: `Notes`,
+        url: `/notes`,
+      },
+    ],
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+    `@pauliescanlon/gatsby-mdx-embed`,
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        basePath: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {
+        basePath: `/notes`,
+      },
+    },
+  ],
 }
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+export default function BioContent() {
+  return (
+    <Fragment>
+      Words by <Styled.a href="http://example.com/">Your Name</Styled.a>.
+      <br />
+      Change me. Your awesome bio, about how great you are!
+      This is my first shadowing.
+    </Fragment>
+  )
+}
